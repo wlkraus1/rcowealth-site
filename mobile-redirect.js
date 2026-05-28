3a3c842fa9c833874bc83ae7416769112b188c3d
Tyler Krause <t.krause@mackbook.tail34488a.ts.net>
2026-05-27 23:14:20 -0400

    Allow life landing pages on mobile

--- mobile-redirect.js
+++ mobile-redirect.js
@@ -1,6 +1,11 @@
 (function(){
   var path = window.location.pathname || '/';
   if (path.indexOf('/mobile.html') !== -1 || path.indexOf('/thank-you') !== -1) return;
+  if (
+    path.indexOf('/life-insurance-protection-review.html') !== -1 ||
+    path.indexOf('/life-insurance-review-checklist.html') !== -1 ||
+    path.indexOf('/life-insurance-greenville-sc.html') !== -1
+  ) return;
   var isMobileWidth = window.matchMedia && window.matchMedia('(max-width: 820px)').matches;
   var isCoarse = window.matchMedia && window.matchMedia('(pointer: coarse)').matches;
   var isMobileUA = /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent || '');
